1c0181fa17705b245f88fcc0315f70b8650b048f
zk <liuzq7@gmail.com>
2023-04-03 16:02:35 +0800

    feat: add useWindowSize

---
+++ readme.md
@@ -0,0 +1,6 @@
+# useDPR && useWindowSize
+caculate image size  
+# features
+1. monitor DPR(window.devicePixelRatio) changed.
+2. monitor window width 
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState, useMemo } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import useDpr from "./useDPR";
+import useWindowSize from "./useWindowSize";
 
 function App() {
 	const dpr = useDpr();
+  const size = useWindowSize()
 	const src = useMemo(() => {
-		return "https://dummyimage.com/" + 480 * dpr;
-	}, [dpr]);
+		return "https://dummyimage.com/" + size.width * dpr;
+	}, [dpr,size]);
 	return (
 		<div className="App">
 			dpr is: {dpr}
-			<img style={{width:"480px"}} src={ src } />
+      size is: {size.width}
+			<img style={{width:"1200px"}} src={ src } />
+			<img src={ src } />
 		</div>
 	);
 }
