4ed3572a384d334672a4bf11741fed12da78f90f
zk <liuzq7@gmail.com>
2023-04-03 18:29:42 +0800

    chore: more demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useEffect, useState, useMemo } from "react";
 import "./App.css";
 import useDpr from "./useDPR";
-import useWindowSize from "./useWindowSize";
 import ExactImage from './ExactImage'
 
 function App() {
 
+  const dpr = useDpr();
 	return (
 		<div className="App">
+      DPR: {dpr} <br/>
 	      <ExactImage width={200} src={(imageWidth,dpr)=>{		return "https://dummyimage.com/" +  imageWidth * dpr 	  }} />
+    <div style={{width:'200px',height:'10px',background:'red'}}></div>
 		  <ExactImage width={400} src={(imageWidth,dpr)=>{		return "https://picsum.photos/id/237/" +  imageWidth * dpr 	  }} />
 
+    <div style={{width:'400px',height:'10px',background:'green'}}></div>
 		</div>
 	);
 }
